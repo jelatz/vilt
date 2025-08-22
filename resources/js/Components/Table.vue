2d63706f2d1f1bbe109e11e0f7de522c220f69f2
Jelatz <lanetejlad@gmail.com>
2025-08-22 23:31:57 +0800

    created a table component and added to dashboard

---
+++ resources/js/Components/Table.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="overflow-x-auto">
+        <table class="w-full border-collapse bg-gray-100">
+            <thead class="sticky text-left bg-gray-200">
+                <tr>
+                    <!-- Use slots for header -->
+                    <slot name="head" />
+                </tr>
+            </thead>
+            <tbody>
+                <!-- Slot for rows -->
+                <slot name="body" />
+            </tbody>
+            <tfoot>
+                <slot name="foot" />
+            </tfoot>
+        </table>
+    </div>
+</template>
+
+<script setup>
+</script>
+
+<style scoped>
+:deep(table>thead>tr>th),
+:deep(table>tbody>tr>td) {
+    padding: 0.5rem;
+    border-bottom: 1px solid #eee;
+}
+
+:deep(table>tbody>tr:hover) {
+    background-color: gray;
+    color: white;
+}
+</style>
--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -1,11 +1,35 @@
 <template>
-    <div>
-        123123
+    <div class="container mx-auto mt-5">
+        <Table>
+            <!-- Header -->
+            <template #head>
+                <th class="w-[80%]">User</th>
+                <th class="w-[20%]">Action</th>
+            </template>
+
+            <!-- Body -->
+            <template #body>
+                <tr>
+                    <td class="w-[80%]">John</td>
+                    <td class="w-[20%]">Delete</td>
+                </tr>
+                <tr>
+                    <td class="w-[80%]">Jane</td>
+                    <td class="w-[20%]">Edit</td>
+                </tr>
+            </template>
+            <template #foot>
+                <tr>
+                    <td colspan="2" class="text-center">End of Table</td>
+                </tr>
+
+            </template>
+        </Table>
     </div>
 </template>
 
 <script setup>
-
+import Table from '@/Components/Table.vue'
 </script>
 
 <style lang="scss" scoped></style>
